d875bc44f064d63619fbbc2aaa69845dda7a1b2e
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-04-04 12:14:47 +0900

    canEditTodo&canDeleteTodoというフラグの削除 & それに伴う条件分岐の見直し & 不要な関数の削除

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = Vue.createApp({
     return {
       newTodo: "",
       todos: JSON.parse(localStorage.getItem("vue-todos") || "[]"),
-      canEditTodo: true,
-      canDeleteTodo: true,
       editedTodo: "",
     };
   },
@@ -43,20 +41,10 @@ const app = Vue.createApp({
     },
     editTodo(todo) {
       this.editedTodo = { ...todo };
-      this.disableEditAndDeleteTodo();
-    },
-    disableEditAndDeleteTodo() {
-      this.canEditTodo = false;
-      this.canDeleteTodo = false;
-    },
-    enableEditAndDeleteTodo() {
-      this.canEditTodo = true;
-      this.canDeleteTodo = true;
     },
     cancelEdit(todo, index) {
       const savedTodo = JSON.parse(localStorage.getItem("vue-todos"))[index];
       todo.title = savedTodo.title;
-      this.enableEditAndDeleteTodo();
       this.editedTodo = "";
       this.saveTodos();
     },
@@ -68,16 +56,14 @@ const app = Vue.createApp({
           (todo) => todo.id === this.editedTodo.id
         );
         selectedTodo.title = this.editedTodo.title;
-        this.enableEditAndDeleteTodo();
-        this.editedTodo = "";
+          this.editedTodo = "";
         this.saveTodos();
       }
     },
     deleteTodo(index) {
       if (window.confirm("削除してもよろしいでしょうか？")) {
         this.todos.splice(index, 1);
-        this.enableEditAndDeleteTodo();
-        this.saveTodos();
+          this.saveTodos();
       }
     },
   },
